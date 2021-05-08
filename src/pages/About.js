c07e74a10c5998b3f8d9807da80e58bd85033efc
Sandeep <sandeepn@bu.edu>
2021-05-08 12:33:46 -0400

    Fix build issues

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,7 +15,9 @@ const About = (props) => {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getAboutData(), []);
+  useEffect(() => 
+  // eslint-disable-line react-hooks/exhaustive-deps
+  getAboutData(), []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => (
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,14 +15,16 @@ const Projects = (props) => {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
-  useEffect(() => getProjectsData(), []);
+  useEffect(() => 
+  // eslint-disable-line react-hooks/exhaustive-deps
+  getProjectsData(), []);
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
     return projects.map((project) => (
       <div>
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        <img src={project.image} alt="Github"/>
         <a href={project.git}>
           <button>Github</button>
         </a>
